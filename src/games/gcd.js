4fba3565aede4c44acd07b0884f95f8a2741f6b3
romaverne <roman@oyster-media.ru>
2023-07-22 18:11:26 +0300

    brain gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,30 @@
+import { getUserName, responseCheck } from '../index.js';
+
+const getNod = (a, b) => {
+  let x = a;
+  let y = b;
+  while (x !== 0 && y !== 0) {
+    if (x > y) {
+      x %= y;
+    } else {
+      y %= x;
+    }
+  }
+
+  return a + b;
+};
+const randomQuestion = (func) => {
+  const a = func(0, 100);
+  const b = func(a, 100 + a);
+  const answer = getNod(a, b);
+
+  return { question: `${a} ${b}`, trueAnswer: String(answer) };
+};
+
+const gcd = () => {
+  const name = getUserName();
+  console.log('Find the greatest common divisor of given numbers.');
+
+  responseCheck(name, randomQuestion);
+};
+export default gcd;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import readlineSync from 'readline-sync';
 
 export const getUserName = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('May I have your name?');
+  console.log('Welcome to the Brain Games!\nMay I have your name?');
   const name = readlineSync.question('Your answer: ');
   console.log(`Hello, ${name}!`);
   return name;
 };
 export const getRandomArbitrary = (min, max) => Math.floor(Math.random() * (max - min) + min);
 export const responseCheck = (name, randomQuestion, roundValue = 0, maxRoundValue = 3) => {
-  const val = randomQuestion(getRandomArbitrary);
-  const answer = readlineSync.question(`Question: ${val.question}\nYour answer: `);
+  const question = randomQuestion(getRandomArbitrary);
+  const answer = readlineSync.question(`Question: ${question.question}\nYour answer: `);
 
-  if (answer === val.trueAnswer) {
+  if (answer === question.trueAnswer) {
     console.log('Correct!');
 
     const sumRound = roundValue + 1;
@@ -22,6 +21,6 @@ export const responseCheck = (name, randomQuestion, roundValue = 0, maxRoundValu
       console.log(`Congratulations, ${name}!`);
     }
   } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${val.trueAnswer}'.\nLet's try again, ${name}!`);
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${question.trueAnswer}'.\nLet's try again, ${name}!`);
   }
 };
